a21fbd2fa7bd79aa693d97b4226d8fc56da42a60
hassnaa <hassnaamahmoud96@gmail.com>
2024-06-20 17:24:12 +0200

    push js files

---
+++ js/master.js
@@ -0,0 +1,317 @@
+let mainColors = localStorage.getItem("color_option");
+
+if (mainColors !== null) {
+  document.documentElement.style.setProperty("--main-color", mainColors);
+
+  document.querySelectorAll(".colors-list li").forEach((element) => {
+    element.classList.remove("active");
+
+    if (element.dataset.color === mainColors) {
+      element.classList.add("active");
+    }
+  });
+}
+
+// Random Background Option
+let backgroundOption = true;
+
+let backgroundInterval;
+
+let backgroundLocalItem = localStorage.getItem("background_option");
+
+if (backgroundLocalItem !== null) {
+  document.querySelectorAll(".random-backgrounds span").forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  if (backgroundLocalItem === "true") {
+    backgroundOption = true;
+
+    document.querySelector(".random-backgrounds .yes").classList.add("active");
+  } else {
+    backgroundOption = false;
+
+    document.querySelector(".random-backgrounds .no").classList.add("active");
+  }
+}
+
+// Click On Toggle Settings Gear
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+  this.classList.toggle("fa-spin");
+
+  document.querySelector(".settings-box").classList.toggle("open");
+};
+
+// Switch Colors
+const colorsLi = document.querySelectorAll(".colors-list li");
+
+colorsLi.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+
+    localStorage.setItem("color_option", e.target.dataset.color);
+
+    handleActive(e);
+  });
+});
+
+// Switch Random Background Option
+const randomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+randomBackEl.forEach((span) => {
+  span.addEventListener("click", (e) => {
+    handleActive(e);
+
+    if (e.target.dataset.background === "yes") {
+      backgroundOption = true;
+
+      randomizeImgs();
+
+      localStorage.setItem("background_option", true);
+    } else {
+      backgroundOption = false;
+
+      clearInterval(backgroundInterval);
+
+      localStorage.setItem("background_option", false);
+    }
+  });
+});
+
+// Select Landing Page Element
+let landingPage = document.querySelector(".landing-page");
+
+let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+function randomizeImgs() {
+  if (backgroundOption === true) {
+    backgroundInterval = setInterval(() => {
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+
+      // Change Background Image Url
+      landingPage.style.backgroundImage =
+        'url("imgs/' + imgsArray[randomNumber] + '")';
+    }, 10000);
+  }
+}
+
+randomizeImgs();
+
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
+window.onscroll = function () {
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
+
+// Create Popup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // Create Overlay Element
+    let overlay = document.createElement("div");
+
+    // Add Class To Overlay
+    overlay.className = "popup-overlay";
+
+    // Append Overlay To The Body
+    document.body.appendChild(overlay);
+
+    // Create The Popup Box
+    let popupBox = document.createElement("div");
+
+    // Add Class To The Popup Box
+    popupBox.className = "popup-box";
+
+    if (img.alt !== null) {
+      // Create Heading
+      let imgHeading = document.createElement("h3");
+
+      // Create text For Heading
+      let imgText = document.createTextNode(img.alt);
+
+      // Append The Text To The Heading
+      imgHeading.appendChild(imgText);
+
+      // Append The Heading To The Popup Box
+      popupBox.appendChild(imgHeading);
+    }
+
+    // Create The Image
+    let popupImage = document.createElement("img");
+
+    // Set Image Source
+    popupImage.src = img.src;
+
+    // Add Image To Popup Box
+    popupBox.appendChild(popupImage);
+
+    // Append The Popup Box To Body
+    document.body.appendChild(popupBox);
+
+    // Create The Close Span
+    let closeButton = document.createElement("span");
+
+    // Create The Close Button Text
+    let closeButtonText = document.createTextNode("X");
+
+    // Append Text To Close Button
+    closeButton.appendChild(closeButtonText);
+
+    // Add Class To Close Button
+    closeButton.className = "close-button";
+
+    // Add Close Button To The Popup Box
+    popupBox.appendChild(closeButton);
+  });
+});
+
+// Close Popup
+document.addEventListener("click", function (e) {
+  if (e.target.className == "close-button") {
+    // Remove The Current Popup
+    e.target.parentNode.remove();
+
+    // Remove Overlay
+    document.querySelector(".popup-overlay").remove();
+  }
+});
+
+// Select All Bullets
+const allBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+// Select All Links
+const allLinks = document.querySelectorAll(".links a");
+
+function scrollToSomewhere(elements) {
+  elements.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      document.querySelector(e.target.dataset.section).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+scrollToSomewhere(allBullets);
+scrollToSomewhere(allLinks);
+
+// Handle Active State
+function handleActive(ev) {
+  // Remove Active Class From All Childrens
+  ev.target.parentElement.querySelectorAll(".active").forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  // Add Active Class On Self
+  ev.target.classList.add("active");
+}
+
+let bulletsSpan = document.querySelectorAll(".bullets-option span");
+
+let bulletsContainer = document.querySelector(".nav-bullets");
+
+let bulletLocalItem = localStorage.getItem("bullets_option");
+
+if (bulletLocalItem !== null) {
+  bulletsSpan.forEach((span) => {
+    span.classList.remove("active");
+  });
+
+  if (bulletLocalItem === "block") {
+    bulletsContainer.style.display = "block";
+
+    document.querySelector(".bullets-option .yes").classList.add("active");
+  } else {
+    bulletsContainer.style.display = "none";
+
+    document.querySelector(".bullets-option .no").classList.add("active");
+  }
+}
+
+bulletsSpan.forEach((span) => {
+  span.addEventListener("click", (e) => {
+    if (span.dataset.display === "show") {
+      bulletsContainer.style.display = "block";
+
+      localStorage.setItem("bullets_option", "block");
+    } else {
+      bulletsContainer.style.display = "none";
+
+      localStorage.setItem("bullets_option", "none");
+    }
+
+    handleActive(e);
+  });
+});
+
+// Reset Button
+document.querySelector(".reset-options").onclick = function () {
+  // localStorage.clear();
+  localStorage.removeItem("color_option");
+  localStorage.removeItem("background_option");
+  localStorage.removeItem("bullets_option");
+
+  // Reload Window
+  window.location.reload();
+};
+
+// Toggle Menu
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function (e) {
+  // Stop Propagation
+  e.stopPropagation();
+
+  // Toggle Class "menu-active" On Button
+  this.classList.toggle("menu-active");
+
+  // Toggle Class "open" On Links
+  tLinks.classList.toggle("open");
+};
+
+// Click Anywhere Outside Menu And Toggle Button
+document.addEventListener("click", (e) => {
+  if (e.target !== toggleBtn && e.target !== tLinks) {
+    // Check If Menu Is Open
+    if (tLinks.classList.contains("open")) {
+      // Toggle Class "menu-active" On Button
+      toggleBtn.classList.toggle("menu-active");
+
+      // Toggle Class "open" On Links
+      tLinks.classList.toggle("open");
+    }
+  }
+});
+
+// Stop Propagation On Menu
+tLinks.onclick = function (e) {
+  e.stopPropagation();
+};
